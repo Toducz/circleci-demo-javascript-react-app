166eea502e2eb615b70909d9ca6da6a8a4f655fb
Toducz <64789799+Toducz@users.noreply.github.com>
2022-12-21 16:59:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        <Header title="Baby Hippo Endre 123asd47"/>
+        <Header title="Baby Hippo Endre 123asd478"/>
         <Container>
           <Card cards={ this.state.cards } />
         </Container>
